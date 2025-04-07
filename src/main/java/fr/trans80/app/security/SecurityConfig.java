22c1eea7603b8923fc0dbe9956ecba15e2aea518
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2025-04-07 17:23:53 +0200

    feat: Add SecurityConfig for OAuth2 login and configure security settings

---
+++ src/main/java/fr/trans80/app/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package fr.trans80.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/").permitAll()
+//                        .requestMatchers("/routes").authenticated()
+                        .anyRequest().permitAll()
+                )
+                // Configures OAuth2 login settings
+//                .oauth2Login(oauth2 -> oauth2
+//                        .loginPage("/oauth2/authorization/keycloak")
+//                        .defaultSuccessUrl("/menu", true)
+//                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                );
+
+        return http.build();
+    }
+}
